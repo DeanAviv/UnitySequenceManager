d0d50ce155aee70ccbddeff0d413ed54c8085d85
Dean Aviv <77728136+DeanAviv@users.noreply.github.com>
2024-07-16 16:31:14 +0300

    added custom attribute for sequence

---
+++ Scripts/SequenceAttribute.cs
@@ -0,0 +1,9 @@
+namespace UnitySequenceManager
+{
+    using System;
+
+    [AttributeUsage(AttributeTargets.Class, Inherited = true, AllowMultiple = false)]
+    public class SequenceAttribute : Attribute
+    {
+    }
+}
